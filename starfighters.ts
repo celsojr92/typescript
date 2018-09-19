01c9078bc7ea8cbd9f3f9f923b682502eb2441c5
Celso Junior <celsobatistajr@gmail.com>
2018-09-19 12:43:39 -0300

    initial commit

---
+++ starfighters.ts
@@ -0,0 +1,21 @@
+import { Spacecraft, ContainerShip } from './base-ships';
+
+class MilleniumFalcon extends Spacecraft implements ContainerShip {
+
+    cargoContainers: number;
+
+    constructor() {
+        super('hyperdrive');
+        this.cargoContainers = 4;
+    }
+
+    jumpIntoHyperspace() {
+        if (Math.random() >= 0.5) {
+            super.jumpIntoHyperspace()
+        } else {
+            console.log('Failed to jump into hyperspace');
+        }
+    }
+}
+
+export { MilleniumFalcon };
